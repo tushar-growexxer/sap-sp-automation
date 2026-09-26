1c22c087f721f19121ff42692304a0ea199358a1
Om Mehta <om.mehta@growexx.com>
2026-09-26 23:00:06 +0530

    SPs Updated : 26-09-2026 11:00 PM IST

--- data/MILIVE/SBO_SP_TRANSACTIONNOTIFICATION.sql
+++ data/MILIVE/SBO_SP_TRANSACTIONNOTIFICATION.sql
@@ -7076,7 +7076,7 @@ If :object_type = '20' and (:transaction_type = 'A' OR :transaction_type = 'U') 
 	END IF;
 END IF;
 
-IF object_type = '20' AND (:transaction_type = 'A' ) THEN
+/*IF object_type = '20' AND (:transaction_type = 'A' ) THEN
 DECLARE DateGRN1 date;
 Declare Seris varchar(100);
 Declare ItemC varchar(100);
@@ -7100,7 +7100,7 @@ DECLARE MAXXIT int;
 			END IF;
 		MINNIT = MINNIT + 1;
 		END WHILE;
-END IF;
+END IF;*/
 
 IF object_type = '59' AND (:transaction_type = 'A' OR :transaction_type = 'U') THEN
 
